a7cf73813098648e0653196dfecfeb32ca007c04
Douglas costa <douglasalgustocosta@gmail.com>
2019-07-11 11:53:56 -0300

    configuração yarn

--- desafio/.eslintrc.js
+++ desafio/.eslintrc.js
@@ -3,23 +3,21 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    'airbnb',
-  ],
+  extends: ['airbnb-base','prettier'],
+  plugins: 'prettier',
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
   rules: {
+    "prettier/prettier": "error", 
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", {"argsIgnorePatten": "next"}],
   },
 };
