0940cbe49ffea3afa917e8df076d4a2f2450dcf4
Douglas costa <douglasalgustocosta@gmail.com>
2019-07-11 13:45:02 -0300

    sequelize

---
+++ desafio/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports ={
+    dialect : 'postgres',
+    host: 'tuffi.db.elephantsql.com',
+    username: 'jgkjhupi',
+    password: 'qJgYoUhhVK-_D_jWAsXuyur2QlRCm0yz',
+    database : 'jgkjhupi',
+    define:{
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+};
